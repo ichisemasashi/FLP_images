56db9edf7c6fcbe523be0e7382e7f7a9582b61cd
ichisemasashi <ichise.masashi@gmail.com>
2021-04-29 06:49:54 +0900

    first commit for clojure project

---
+++ get_imgs/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to get_imgs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ get_imgs/test/get_imgs/core_test.clj
@@ -0,0 +1,7 @@
+(ns get-imgs.core-test
+  (:require [clojure.test :refer :all]
+            [get-imgs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
